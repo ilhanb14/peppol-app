e2b0c3a1ff592529a50ce40e27140442c6af4b51
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-05-05 14:24:08 +0200

    completed input form

--- form.php
+++ form.php
@@ -1,3 +1,4 @@
-<form>
-    <input type="file" id="fileInput" name="files[]">
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" id="fileInput" name="files[]" multiple>
+    <input type="submit" value="Upload" name="submit">
 </form>
